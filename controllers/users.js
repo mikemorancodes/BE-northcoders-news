8ecaa1e38db9a28fc65c360c9312651692d12d28
mikemorancodes <mikemoranmusic@gmail.com>
2018-05-19 13:13:17 +0100

    omit password from user response

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,18 @@
 const { User } = require("../models");
 
 exports.getUserByUsername = (req, res, next) => {
-  User.findOne({ username: req.params.username }).then(
-    user => (user ? res.send({ user }) : next({ status: 400, message: "invalid username" }))
-  );
+  User.findOne({ username: req.params.username })
+    .select("-password")
+    .then(
+      user => (user ? res.send({ user }) : next({ status: 400, message: "invalid username" }))
+    );
 };
 
 exports.createUser = (req, res, next) => {
   const user = new User(req.body);
   user
     .save()
-    .then(user => User.findOne({ _id: user._id }))
+    .then(user => User.findOne({ _id: user._id }).select("-password"))
     .then(user => res.status(201).send({ user }))
     .catch(err => next({ status: 400, message: "invalid parameters" }));
 };
